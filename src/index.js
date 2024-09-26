a8181613b2c32922ef8df66fbabe09c13c7752fb
malory171 <malory171@gmail.com>
2024-09-26 19:25:13 +0200

    Added basic form handling & typewriter plug in.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+function generateCountryInformation(event) {
+  event.preventDefault();
 
+  new Typewriter("#country-information", {
+    strings: "<h2>Country Overview</h2>",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let countryInformationFormElement = document.querySelector(
+  "#country-information-generator-form"
+);
+countryInformationFormElement.addEventListener(
+  "submit",
+  generateCountryInformation
+);
